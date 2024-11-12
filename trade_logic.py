ca162c4b3fed65ddc419d9cd2f171b4910e6398d
SVSKHD <8svskhd@gmail.com>
2024-11-12 07:08:28 +0530

    pip difference

--- trade_logic.py
+++ trade_logic.py
@@ -1,7 +1,6 @@
 def calculate_pip_difference(symbol, current_price, start_price):
-
     result = current_price - start_price
     formatted_difference = result / symbol['pip_size']
     threshold_reached = formatted_difference/symbol[""]
     data = {'symbol': symbol["symbol"], 'pip_difference': result, 'format_difference': formatted_difference, 'thresholds':threshold_reached}
-
+    return data
