5ad06eef7fec7a4f75550115659ee1afb3ab567c
hitheshsvsk8 <hithesh.sunkara@negroup.com>
2024-11-19 18:14:58 +0530

    trade_placement_pending

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ from fetch_prices import fetch_current_price, fetch_price
 import asyncio
 from trade_logic_normal import process_prices_with_hedging
 
-def check_data(data):
-    print("data", data)
+def check_thresholds(data):
+    symbol_name = data['symbol']
+    direction = data['direction']
+    threshold = data['threshold']
+    current_price = data['current_price']
+    positive_threshold_reached = data['positive_threshold_reached']
+    positive_hedging_activated = data['positive_hedging_activated']
+    negative_threshold_reached = data['negative_threshold_reached']
+    negative_hedging_activated = data['negative_hedging_activated']
+    if positive_threshold_reached:
+        if threshold>1:
+            print(f"Positive threshold reached for {symbol_name} at {current_price} with threshold {threshold}.")
+    elif negative_threshold_reached:
+        if threshold<-1:
+            print(f"Negative threshold reached for {symbol_name} at {current_price}. with threshold {threshold}")
 
 async def main():
     connect = await connect_mt5()
@@ -28,11 +41,10 @@ async def main():
                 if current_price is not None:
                     data['current_price'] = current_price
                     # print(f"Symbol: {data['symbol']}, Current Price: {current_price} Start price : {round(data['start_price'],4)} {data}")
-                    check_data(data)
                     symbol = next((s for s in symbols_config if s['symbol'] == data['symbol']), None)
                     if symbol:
-                        thresholds = process_prices_with_hedging(symbol, data['current_price'], data['start_price'])
-                        print("thresholds", thresholds)
+                        threshold_triggered = process_prices_with_hedging(symbol, data['current_price'], data['start_price'])
+                        check_thresholds(threshold_triggered)
             await asyncio.sleep(1)  # Wait for 1 second before fetching the prices again
 
 
--- trade_logic_normal.py
+++ trade_logic_normal.py
@@ -38,32 +38,36 @@ def process_prices_with_hedging(symbol, current_price, start_price):
     data = calculate_pip_difference(symbol, current_price, start_price)
     direction = data['direction']
 
-    print(f"Debug: Evaluating price {current_price}, Direction: {direction}, Threshold Difference: {data['format_symbol_pip_difference']}")
+    # print(f"Debug: Evaluating price {current_price}, Direction: {direction}, Threshold Difference: {data['format_symbol_pip_difference']}")
 
     if not state.positive_threshold and data['format_symbol_pip_difference'] >= 1:
         state.update(positive_threshold=True, positive_threshold_price=current_price)
-        print(f"Positive threshold reached for {data['symbol']} at {current_price}.")
+        # print(f"Positive threshold reached for {data['symbol']} at {current_price}.")
 
     if state.positive_threshold and not state.positive_hedging and data['format_symbol_pip_difference'] <= 0.5:
         state.update(positive_hedging=True, positive_hedging_price=current_price)
-        print(f"Positive hedging activated for {data['symbol']} at {current_price}.")
+        # print(f"Positive hedging activated for {data['symbol']} at {current_price}.")
 
     if not state.negative_threshold and data['format_symbol_pip_difference'] <= -1:
         state.update(negative_threshold=True, negative_threshold_price=current_price)
-        print(f"Negative threshold reached for {data['symbol']} at {current_price}.")
+        # print(f"Negative threshold reached for {data['symbol']} at {current_price}.")
 
     if state.negative_threshold and not state.negative_hedging and data['format_symbol_pip_difference'] > -0.5:
         state.update(negative_hedging=True, negative_hedging_price=current_price)
-        print(f"Negative hedging activated for {data['symbol']} at {current_price}.")
+        # print(f"Negative hedging activated for {data['symbol']} at {current_price}.")
 
-    print(
-        f"symbol: {symbol['symbol']} | direction: {direction} | "
-        f"threshold: {round(data['format_symbol_pip_difference'], 2)} | "
-        f"current_price: {data['current_price']} | "
-        f"positive_threshold_reached: {state.positive_threshold} | "
-        f"positive_hedging_activated: {state.positive_hedging} | "
-        f"negative_threshold_reached: {state.negative_threshold} | "
-        f"negative_hedging_activated: {state.negative_hedging}"
-    )
+    message = f"symbol: {symbol['symbol']} | direction: {direction} | threshold: {round(data['format_symbol_pip_difference'], 2)} | current_price: {data['current_price']} | positive_threshold_reached: {state.positive_threshold} | positive_hedging_activated: {state.positive_hedging} | negative_threshold_reached: {state.negative_threshold} | negative_hedging_activated: {state.negative_hedging}"
+    data = {
+        'symbol': symbol['symbol'],
+        'direction': direction,
+        'threshold': round(data['format_symbol_pip_difference'], 2),
+        'current_price': data['current_price'],
+        'positive_threshold_reached': state.positive_threshold,
+        'positive_hedging_activated': state.positive_hedging,
+        'negative_threshold_reached': state.negative_threshold,
+        'negative_hedging_activated': state.negative_hedging
+    }
+    # print(message)
+    return data
 
 
