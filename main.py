909db9398436123defbf5984e68b1814d9f802d1
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2024-11-25 00:52:20 +0530

    logic and main fix

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from numpy.lib.function_base import place
+
 from config import symbols_config
-from storage_state import save_symbol_data, delete_symbol_data, get_symbol_data, update_symbol_data
+from storage_state import get_symbol_data
 from utils import connect_mt5
 from fetch_prices import fetch_price
+from trade_placement import place_trade_notify, hedge_place_trade, close_trades_by_symbol
 import asyncio
 from logic import analyze_pip_difference
 
@@ -9,7 +12,6 @@ from logic import analyze_pip_difference
 def check_thresholds_and_trade(symbol):
     get_data = get_symbol_data(symbol["symbol"])
     if get_data:
-        print("get_data", get_data['symbol'])
         start_price = get_data['start_price']
         current_price = get_data['current_price']
         # positive aspects
@@ -30,18 +32,22 @@ def check_thresholds_and_trade(symbol):
 
         if positive_threshold:
             print("positive_threshold", positive_threshold, positive_threshold_price)
+            place_trade_notify(symbol, "sell", 1.0)
         if negative_threshold:
             print("negative_threshold", negative_threshold, negative_threshold_price)
-
+            place_trade_notify(symbol, "buy", 1.0)
         if positive_second_threshold:
             print("positive_second_threshold", positive_second_threshold, positive_second_threshold_price)
+            close_trades_by_symbol(symbol)
         if negative_second_threshold:
             print("negative_second_threshold", negative_second_threshold, negative_second_threshold_price)
-
+            close_trades_by_symbol(symbol)
         if positive_hedging:
             print("positive_hedging", positive_hedging, positive_hedging_price)
+            hedge_place_trade(symbol, "sell", 1.0)
         if negative_threshold:
             print("negative_threshold", negative_threshold, negative_threshold_price)
+            hedge_place_trade(symbol, "buy", 1.0)
 
         # current_price = get_data['current_price']
         # symbol = next((s for s in symbols_config if s['symbol'] == symbol), None)
@@ -53,7 +59,7 @@ async def main():
     if connect:
         price_data = []
         for symbol in symbols_config:
-            start_price = await fetch_price(symbol['symbol'], "start")
+            start_price = await fetch_price(symbol, "start")
             if start_price is not None:
                 price_data.append({
                     'symbol': symbol['symbol'],
@@ -62,14 +68,11 @@ async def main():
                 })
         while True:
             for data in price_data:
-                current_price = await fetch_price(data['symbol'], "current")
+                current_price = await fetch_price(data, "current")
                 if current_price is not None:
                     data['current_price'] = current_price
                     symbol = next((s for s in symbols_config if s['symbol'] == data['symbol']), None)
-                    print("symbol", symbol)
-                    threshold_calculator = analyze_pip_difference(symbol, data['start_price'], data['current_price'])
-
-                    print("threshold_calculator", threshold_calculator)
+                    analyze_pip_difference(symbol, data['start_price'], data['current_price'])
             await asyncio.sleep(1)
 
 
